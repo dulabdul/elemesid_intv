359c446f5566c28bd80956a996b913adeb376015
dulabdul <dul.abdoel21@gmail.com>
2023-11-12 00:40:11 +0700

    refactor : delete default code

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Category from '@/components/Home/Category';
+import Hero from '@/components/Home/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Hero />
+        <Category />
+      </main>
+    </>
+  );
+}
