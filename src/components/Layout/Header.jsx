165db455a592d0cffb845b801a5f45308161e878
dulabdul <dul.abdoel21@gmail.com>
2023-11-12 00:39:25 +0700

    feat(UI):add header component

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header className='p-7 w-full '>
+      <nav className='flex items-center flex-row justify-between navbar'>
+        <div className='navbar_logo'>
+          <Link href={'/'}>
+            <Image
+              priority
+              src={'/images/logo_elemes.png'}
+              alt='Logo Elemes ID'
+              width={150}
+              height={40}
+            />
+          </Link>
+        </div>
+        <div className='navbar_button_sign_up'>
+          <a
+            href='/'
+            className='w-full px-4 py-3 rounded-full bg-primary text-white'>
+            Daftar Sekarang
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
